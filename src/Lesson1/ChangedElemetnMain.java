648040d808dba57937dae3e9f2759df457a3cb96
Vitas1968 <dor1968@mail.ru>
2019-08-20 19:11:14 +0700

    реализация

--- src/Lesson1/ChangedElemetnMain.java
+++ src/Lesson1/ChangedElemetnMain.java
@@ -2,4 +2,22 @@ package Lesson1;
 
 public class ChangedElemetnMain
 {
+    public static void main(String[] args)
+    {
+        char[] charArr = {'a','b','c','d','e'};
+
+            System.out.print(changedElemet(charArr,2,5));
+
+    }
+    public static char[] changedElemet(char[] array, int idx1, int idx2)
+    {
+        if ((idx1>0 && idx1 <array.length) && (idx2>0 && idx2<array.length))
+        {
+            char ch = array[idx1];
+            array[idx1] = array[idx2];
+            array[idx2] = ch;
+        } else
+            {throw new ArrayIndexOutOfBoundsException("Некорректный индекс"); }
+        return array;
+    }
 }

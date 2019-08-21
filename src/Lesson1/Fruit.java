f2372e85b6d143f9f83239036a6ef39f464cb7b7
Vitas1968 <dor1968@mail.ru>
2019-08-21 21:44:13 +0700

    Абстрактный класс Фрукт и два наследника Яблоко и Апельсин

--- src/Lesson1/Fruit.java
+++ src/Lesson1/Fruit.java
@@ -1,5 +1,49 @@
 package Lesson1;
 
-public class Fruit
+public abstract class Fruit
 {
+    private String nameFruit;
+    private float weight;
+
+    public Fruit(String nameFruit)
+    {
+        this.nameFruit=nameFruit;
+    }
+
+    public String getNameFruit()
+    {
+        return nameFruit;
+    }
+
+    public void setNameFruit(String nameFruit)
+    {
+        this.nameFruit = nameFruit;
+    }
+
+    public float getWeight()
+    {
+        return weight;
+    }
+
+    public void setWeight(float weight)
+    {
+        this.weight = weight;
+    }
+}
+
+class Apple extends Fruit
+{
+    public Apple(String nameFruit)
+    {
+        super(nameFruit);
+        setWeight(1.0f);
+    }
+}
+class Orange extends Fruit
+{
+public Orange(String nameFruit)
+{
+    super(nameFruit);
+    setWeight(1.5f);
+}
 }
